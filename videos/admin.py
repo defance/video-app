53e69376b5228882001f181b0a0adbcbaf327e33
Dmitry Ivanyushin <defance@gmail.com>
2017-05-30 10:51:48 +0300

    Админка категорий

--- videos/admin.py
+++ videos/admin.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', )
