fcd2ce7ff4860900a56c7e8ed5bfb37d983ba6b9
Dmitry Ivanyushin <defance@gmail.com>
2017-05-30 13:48:27 +0300

    Локализация полей модели

--- videos/models.py
+++ videos/models.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+from __future__ import unicode_literals
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -14,18 +16,26 @@ class Category(models.Model):
 class Video(models.Model):
 
     STATUS_CHOICES = (
-        ('loading', _(u'Loading')),
-        ('loaded', _(u'Loaded')),
-        ('queued', _(u'Queued')),
-        ('processing', _(u'Processing')),
-        ('ready', _(u'Ready')),
-        ('error', _(u'Error')),
+        ('loading', _('Loading')),
+        ('loaded', _('Loaded')),
+        ('queued', _('Queued')),
+        ('processing', _('Processing')),
+        ('ready', _('Ready')),
+        ('error', _('Error')),
     )
 
-    id = models.CharField(max_length=50, primary_key=True)
-    category = models.ForeignKey(Category)
-    duration = models.CharField(max_length=50, blank=True, null=True)
-    preview = models.ImageField(blank=True, null=True)
-    status = models.CharField(max_length=30, choices=STATUS_CHOICES,
-                              null=True, blank=True)
-    video = models.FileField()
+    id = models.CharField(
+        max_length=50, primary_key=True, verbose_name=_('id')
+    )
+    category = models.ForeignKey(Category, verbose_name=_('Category'))
+    duration = models.CharField(
+        max_length=50, blank=True, null=True, verbose_name=_('Duration')
+    )
+    preview = models.ImageField(
+        blank=True, null=True, verbose_name=_('Preview')
+    )
+    status = models.CharField(
+        max_length=30, choices=STATUS_CHOICES, null=True, blank=True,
+        verbose_name=_('Status')
+    )
+    video = models.FileField(verbose_name=_('Video'))
