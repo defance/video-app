3579706f63d7bdf3f0dd1d0b4895a9e5d5a5a8bf
Dmitry Ivanyushin <defance@gmail.com>
2017-05-31 14:23:30 +0300

    Починен баг с перетиранием поля id

--- videos/static/videos/app.js
+++ videos/static/videos/app.js
@@ -4,10 +4,11 @@ $(function(){
         $form.find('input[name=save_changes]')
             .attr('disabled', 'disabled')
             .val(function(){return $(this).data('msg-uploading')});
-    }).on('click', function(e) {
+    });
+    $('form.upload-video input[type=submit]').on('click', function(e) {
         var $form = $(e.target).closest('form');
         var $id_input = $form.find('input[name=id]');
-        if($id_input.empty()){
+        if($id_input.val() == ''){
             $id_input.val($id_input.attr('placeholder'));
         }
     });
