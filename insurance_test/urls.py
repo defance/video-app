d78db78b57ea2e6083744d51132c044e139fe5c5
Dmitry Ivanyushin <defance@gmail.com>
2017-05-31 13:41:18 +0300

    Директория с закгруженными файлами включена для отладки

--- insurance_test/urls.py
+++ insurance_test/urls.py
@@ -29,3 +29,7 @@ if settings.DEBUG:
     urlpatterns = [
          url(r'^__debug__/', include(debug_toolbar.urls)),
          ] + urlpatterns
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
