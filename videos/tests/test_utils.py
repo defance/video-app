03d0b49187ca6a908ba68159c13f86b7ccd0f863
Dmitry Ivanyushin <defance@gmail.com>
2017-05-31 17:40:07 +0300

    Тесты для get_duration_str

--- videos/tests/test_utils.py
+++ videos/tests/test_utils.py
@@ -2,7 +2,11 @@ from __future__ import unicode_literals
 
 import unittest
 
-from ..utils import extract_raw_duration_info, extract_duration_info
+from django.utils import translation
+from django.utils.translation import pgettext as _p, ugettext as _u
+
+from ..utils import (extract_raw_duration_info, extract_duration_info,
+                     get_duration_str)
 
 
 class TestExtractRawDurationInfo(unittest.TestCase):
@@ -56,3 +60,64 @@ class TestExtractDurationInfo(unittest.TestCase):
             's': float(2.3)
         }
         self.assertDictEqual(expected, extract_duration_info(case))
+
+
+class TestGetDurationStr(unittest.TestCase):
+
+    def test_str_correct(self):
+        case = {'h': 1, 'm': 2, 's': 3.45}
+        expected = " ".join([
+            '1', _p('duration', 'hrs'),
+            '2', _p('duration', 'min'),
+            '3.45', _p('duration', 'sec')
+        ])
+        self.assertEqual(expected, get_duration_str(case))
+
+    def test_something_missing(self):
+        case = {'s': 1.23}
+        expected = " ".join([
+            '1.23', _p('duration', 'sec')
+        ])
+        self.assertEqual(expected, get_duration_str(case))
+
+    def test_something_zero(self):
+        case = {'h': 0, 'm': 0, 's': 1.23}
+        expected = " ".join([
+            '1.23', _p('duration', 'sec')
+        ])
+        self.assertEqual(expected, get_duration_str(case))
+
+    def test_all_zero(self):
+        case = {'h': 0, 'm': 0, 's': 0}
+        expected = _u('Unknown')
+        self.assertEqual(expected, get_duration_str(case))
+
+    def test_all_missing(self):
+        case = {}
+        expected = _u('Unknown')
+        self.assertEqual(expected, get_duration_str(case))
+
+    def test_str_correct_long(self):
+        # seconds will be rounded
+        seconds = 123.4567890
+        case = {'h': 123, 'm': 456, 's': seconds}
+        expected = " ".join([
+            '123', _p('duration', 'hrs'),
+            '456', _p('duration', 'min'),
+            '{:.2f}'.format(seconds), _p('duration', 'sec')
+        ])
+        self.assertEqual(expected, get_duration_str(case))
+
+    def test_str_correct_with_noise(self):
+        case = {'h': 1, 'm': 2, 's': 3.45, 'noise': 'foo'}
+        expected = " ".join([
+            '1', _p('duration', 'hrs'),
+            '2', _p('duration', 'min'),
+            '3.45', _p('duration', 'sec')
+        ])
+        self.assertEqual(expected, get_duration_str(case))
+
+    def test_str_only_noise(self):
+        case = {'noise': 'foo'}
+        expected = _u('Unknown')
+        self.assertEqual(expected, get_duration_str(case))
