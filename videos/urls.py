62b66ce28c7817a47672948a29419c18e10c7229
Dmitry Ivanyushin <defance@gmail.com>
2017-05-31 11:09:46 +0300

    Список видеозаписей

--- videos/urls.py
+++ videos/urls.py
@@ -1,9 +1,11 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import url
+from videos.views import VideosListView
 
 from .views import NewVideoView
 
 urlpatterns = [
+    url(r'^all/$', VideosListView.as_view(), name='all_videos'),
     url(r'^$', NewVideoView.as_view(), name='home'),
 ]
--- videos/views.py
+++ videos/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.urls.base import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 
 from .forms import NewVideoForm
 from .models import Video
@@ -19,6 +20,11 @@ class NewVideoView(CreateView):
         # Create convert task
 
         return super(NewVideoView, self).form_valid(form)
+
+
+class VideosListView(ListView):
+    model = Video
+    context_object_name = "video_list"
 
 
 __all__ = ['NewVideoView']
